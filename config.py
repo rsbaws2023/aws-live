5ee360c6ebb72c67ca91f2b34bd5a7cc4d82606f
rsbaws2023 <145286284+rsbaws2023@users.noreply.github.com>
2023-09-18 11:34:34 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cghrkk3nnfcn.ap-south-1.rds.amazonaws.com" 
+customuser = "employeedb"
+custompass = "password"
+customdb = "employee"
+custombucket = "addemployeedb"
+customregion = "ap-south-1"
